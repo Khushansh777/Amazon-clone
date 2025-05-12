0a5003124bf4d0150eb1653571d15b88f616853d
khushanshbaliyan@gmail.com <khushanshbaliyan@gmail.com>
2025-05-12 10:34:23 +0530

    Removed hardcoded product instance and streamlined product data structure for improved maintainability.

--- data/products.js
+++ data/products.js
@@ -22,22 +22,7 @@ class Product {
     return moneyFn(this.priceCents)
   }
 }
-const product2 =  new Product({
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  })
-    console.log(product2);
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -715,4 +700,3 @@ export const products = [
 ].map((details) => {
     return new Product(details);
 });
-console.log(products);
